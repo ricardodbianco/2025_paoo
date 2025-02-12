c1e86b8117bfc153085f5ad404a2a6043ad0361a
Ricardo Del Bianco <ricardodbianco98@gmail.com>
2025-02-12 18:07:01 -0300

    learn(ap01-js): encerra o codigo 1.5.1
    -falamos sobre vetores
    -falamos sobre coercoes
    -falamos sobre diferencas == e ===

--- intro_js/script.js
+++ intro_js/script.js
@@ -1,32 +1,67 @@
+ //vetores
+ //declaração
+//  v1 = [];
+//  console.log(v1.length);
+//  v1[0] = 3.4;
+//  console.log(v1.length);
+//  v1[10] = 2;
+//  console.log(v1.length);
+//  v1[2] = 'abc';
+//  console.log(v1.length);
+
+//  console.log(v1);
+
+//  for(let i = 0; i < v1.length; i++){
+//     console.log(v1[i]);
+//  }
+ 
+ //comparação com == e ===
+//console.log(1 === '1'); // === comparação mais exata, clara e intuitiva
+//console.log(1 == '1');
+//console.log(1 == 1);
+ 
+ //coerção
+// const n1 = 2;
+// const n2 = '3';
+
+//coerção implicita
+// const n3 = n1 + n2;
+// console.log(n3);
+
+//coerção explícita
+// const n4 = n1 + Number(n2);
+// console.log(n4);
+
+
 //pq não usar o var - é contraintuitivo
-var idade = 18; //var = idade - Declaração da variável idade // = 18 - Atribuição (ordem de execução) da variável idade = 18
-console.log('Oi, ' + nome);
-//içamento (hoist) 
-if(idade >=18){
-    var nome = 'João';
-    console.log('Parabéns, ' + nome + '.Você pode dirigir');
-}
-console.log("Ate mais, " + nome);
+// var idade = 18; //var = idade - Declaração da variável idade // = 18 - Atribuição (ordem de execução) da variável idade = 18
+// console.log('Oi, ' + nome);
+// //içamento (hoist) 
+// if(idade >=18){
+//     var nome = 'João';
+//     console.log('Parabéns, ' + nome + '.Você pode dirigir');
+// }
+// console.log("Ate mais, " + nome);
 
-// var liguaguem = "Javascript";
-// console.log("Aprendendo " + libguagem);
-// var linguagem = "Java";
-// console.log("Aprendendo " + linguagem);
+// // var liguaguem = "Javascript";
+// // console.log("Aprendendo " + libguagem);
+// // var linguagem = "Java";
+// // console.log("Aprendendo " + linguagem);
 
-// //declarando variáveis
-// var b = 2; //é preferível não usar var, apenas let e const
-// console.log(b);
-// b = 3;
-// console.log(b);
+// // //declarando variáveis
+// // var b = 2; //é preferível não usar var, apenas let e const
+// // console.log(b);
+// // b = 3;
+// // console.log(b);
 
-// let a = 2; //let = seja
-// console.log(a);
-// a = 3;
-// console.log(a);
+// // let a = 2; //let = seja
+// // console.log(a);
+// // a = 3;
+// // console.log(a);
 
-// //declarando constantes
-// //Java: String nome = "José";
-// const nome = "José";
-// console.log(nome);
-// // nome = 'Pedro'; //const é uma constante, não pode variar
-// // console.log(nome) 
+// // //declarando constantes
+// // //Java: String nome = "José";
+// // const nome = "José";
+// // console.log(nome);
+// // // nome = 'Pedro'; //const é uma constante, não pode variar
+// // // console.log(nome) 
